12c8f28139d6f37046fea62670b57d50d71ac25e
Helga312 <may.traum@gmail.com>
2020-10-22 14:52:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let openBrackets = [],
+        closeBrackets = [],
+        equalBrackets = [],
+        chars = str.split(""),
+        stack = [],
+        openIndex,
+        closeIndex,
+        openBracket,
+        closeBracket;
+
+    if (str.length % 2 === 1) return false;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        openBrackets.push(bracketsConfig[i][0]);
+        closeBrackets.push(bracketsConfig[i][1]);
+    }
+
+    for (let i = 0; i < openBrackets.length; i++) {
+        if (openBrackets[i] === closeBrackets[i]) {
+            equalBrackets.push(openBrackets[i]);
+        }
+    }
+
+    for (let i = 0; i <= chars.length - 1; i++) {
+        openIndex = openBrackets.indexOf(chars[i]);
+        if (openIndex !== -1) {
+            openBracket = closeBrackets[openIndex];
+            stack.push(openBracket);
+            if (
+                stack[stack.length - 1] === stack[stack.length - 2] &&
+                openBrackets.includes(stack[stack.length - 1])) {
+                stack.pop(); stack.pop();
+            };
+            continue;
+        }
+        closeIndex = closeBrackets.indexOf(chars[i]);
+        if (closeIndex !== -1) {
+            closeBracket = chars[i];
+            openBracket = stack.pop();
+            if (closeBracket !== openBracket) {
+                return false;
+            }
+        }
+    }
+
+    if (stack.length !== 0) {
+        return false;
+    }
+
+    return true;
 }
